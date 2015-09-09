fe6c562f0d07b4a52343d52ad2e0ee5424ed3524
AbyChan <crystal.tyan@gmail.com>
2015-09-09 13:51:25 +0800

    update

--- edit-sword.el
+++ edit-sword.el
@@ -344,4 +344,32 @@ region-end is used."
     (join-line -1))
   (back-to-indentation))
 
+
+;;;================================
+(defun vi-open-line-above ()
+  "Insert a newline above the current line and put point at beginning."
+  (interactive)
+  (unless (bolp)
+    (beginning-of-line))
+  (newline)
+  (forward-line -1)
+  (indent-according-to-mode))
+
+(defun vi-open-line-below ()
+  "Insert a newline below the current line and put point at beginning."
+  (interactive)
+  (unless (eolp)
+    (end-of-line))
+  (newline-and-indent))
+
+(defun vi-open-line (&optional abovep)
+  "Insert a newline below the current line and put point at beginning.
+With a prefix argument, insert a newline above the current line."
+  (interactive "P")
+  (if abovep
+      (vi-open-line-above)
+    (vi-open-line-below)))
+
+
+
 (provide 'edit-sword)
